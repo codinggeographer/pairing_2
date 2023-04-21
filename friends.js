118c5aa77b20848661f9b2e24b5c3eb5d88974a2
Natalie Rodriguez <codinggeographer@gmail.com>
2023-04-20 20:20:51 -0600

    Reverse places array

--- friends.js
+++ friends.js
@@ -4,3 +4,9 @@ console.log(arrays[2]);
 console.log(arrays.at(-1));
 console.log(arrays[0]);
 
+var places = ["Rome", "Hawaii", "Austin", "Antartica", "Bahamas"]
+
+var reversePlaces = places.reverse();
+
+console.log(reversePlaces);
+console.log(places);
